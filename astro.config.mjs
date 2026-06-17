74d7d2d5b417032040f325d2756b6c5413c1ed98
Kanta Sato <163727540+go-kante@users.noreply.github.com>
2026-06-17 13:41:36 +1000

    スマホ修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build
 export default defineConfig({
   // ⚡️ サーバー側でのリアルタイム処理（APIなど）を有効にする設定を追加します
   output: 'server',
 
-  adapter: cloudflare()
+  adapter: cloudflare(),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
